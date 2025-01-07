2d516423fa575b64a0bb33505758ecb722a45e92
Aayush Poudel <110235375+aayushhh44@users.noreply.github.com>
2025-01-07 15:37:00 +0545

    Fix:next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:"export",
-    assetPrefix: "./",
-    basePath: "",
+    assetPrefix: "/",
 };
 
 export default nextConfig;
