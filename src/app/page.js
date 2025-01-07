1ae9ef523068f932b36ddd52fbe90bd42b8a826a
Aayush Poudel <110235375+aayushhh44@users.noreply.github.com>
2025-01-07 14:52:26 +0545

    V1 push

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+
+
+// export const runtime = "edge";
+
+
+export default function Home() {
+  return (
+    <main className="dark:bg-[#1a1a1a]">
+      <Hero/>
+
+    </main>
+  );
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        'source-code': ['Source Code Pro', 'monospace'],
+        'poppins': ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default tailwindConfig;
